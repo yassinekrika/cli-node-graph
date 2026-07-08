e16bc65bf6b3d88ab46651e29bf08571a0f3c9e6
Yassine KRIKA <yassinekrika2016@gmail.com>
2026-07-08 11:36:27 +0200

    github action config

--- visualization/vite.config.ts
+++ visualization/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
+  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
 });
